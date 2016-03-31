7d9a340081e73227fc18702170212ff5dd81f1ef
bcr3ative <paolo.perkovic@gmail.com>
2016-03-31 17:20:03 +0200

    Optimizations and cleanup
    - Added some comments to code
    - Removed support for multiple initiators ( DFT is meant to be run with
    one only )
    - Removed unnecessary 'initiator' node variable, the same can be
    achieved with 'entry' node variable only
    - Removed debug prints as all of this can be run through pymote sim gui
    which is much better and easier for debugging

--- pymote/algorithms/traversal.py
+++ pymote/algorithms/traversal.py
@@ -3,24 +3,23 @@ from pymote.message import Message
 
 
 class DFT(NodeAlgorithm):
+    # algorithm input
     required_params = {'informationKey', }
+    # values that are exposed to other algorithms
     default_params = {'neighborsKey': 'Neighbors'}
 
     def initializer(self):
-        ini_nodes = []
         for node in self.network.nodes():
             node.memory[self.neighborsKey] = node.compositeSensor.read()['Neighbors']
             node.status = 'IDLE'
             if self.informationKey in node.memory:
-                node.status = 'INITIATOR'
-                ini_nodes.append(node)
-        for ini_node in ini_nodes:
-            self.network.outbox.insert(0, Message(header=NodeAlgorithm.INI, destination=ini_node))
+                ini_node = node
+        # if multiple initiators are specified only the last one is initialized
+        ini_node.status = 'INITIATOR'
+        self.network.outbox.insert(0, Message(header=NodeAlgorithm.INI, destination=ini_node))
 
     def initiator(self, node, message):
         node.memory['unvisitedNodes'] = list(node.memory[self.neighborsKey])
-        node.memory['initiator'] = True
-        #print("[INITIATOR] Node {} -> has unvisited nodes {}\n".format(node, node.memory['unvisitedNodes']))
         self.visit(node, message)
 
     def idle(self, node, message):
@@ -28,15 +27,12 @@ class DFT(NodeAlgorithm):
             node.memory['entry'] = message.source
             node.memory['unvisitedNodes'] = list(node.memory[self.neighborsKey])
             node.memory['unvisitedNodes'].remove(message.source)
-            node.memory['initiator'] = False
-            #print("[IDLE=T] Node {} -> has unvisited nodes {}\n".format(node, node.memory['unvisitedNodes']))
             self.visit(node, message)
 
     def visited(self, node, message):
         if message.header == 'T':
             node.memory['unvisitedNodes'].remove(message.source)
             node.send(Message(destination=message.source, header='Backedge', data=message.data))
-            #print("[VISITED=T] Node {} -> has unvisited nodes {}, sending Backedge to {}\n".format(node, node.memory['unvisitedNodes'], message.source))
         elif message.header == 'Return':
             node.memory['unvisitedNodes'].remove(message.source)
             self.visit(node, message)
@@ -51,13 +47,10 @@ class DFT(NodeAlgorithm):
         if node.memory['unvisitedNodes']:
             next_node = node.memory['unvisitedNodes'][0]
             node.send(Message(destination=next_node, header='T', data=message.data))
-            #print("[VISIT] Node {} -> has unvisited nodes {}, sending T to {}, status to VISITED\n".format(node, node.memory['unvisitedNodes'], next_node))
             node.status = 'VISITED'
         else:
-            if not node.memory['initiator']:
+            if 'entry' in node.memory:
                 node.send(Message(destination=node.memory['entry'], header='Return', data=message.data))
-                #print("[VISIT=not initiator] Node {} -> sending Return to {}".format(node, node.memory['entry']))
-            #print("[VISIT=no unvisited nodes] Node {} -> status to DONE".format(node))
             node.status = 'DONE'
 
     STATUS = {
